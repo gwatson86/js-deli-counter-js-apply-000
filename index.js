25117fb543b1f225fc6a1d403e41f4a064541829
Graham Watson <gwatson86@gmail.com>
2018-08-09 01:37:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return "Now serving " + katzDeliLine.shift + ".";
+  }
+}
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+}
