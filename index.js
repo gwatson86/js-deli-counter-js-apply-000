60f784c79940e0d5508e78652f3bf289dd93a532
Graham Watson <gwatson86@gmail.com>
2018-08-09 01:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    return ""
+  }
+}
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
